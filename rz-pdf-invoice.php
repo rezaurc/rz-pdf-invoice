9c50c0898527a9f4cd935b6f20f364e381e300d3
rezaurc@gmail.com <rezaurc@gmail.com@b93cb1ed-a8bd-facd-164d-d9cb5ce901ef>
2012-03-23 13:19:52 +0000

    main plugin file

---
+++ rz-pdf-invoice.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: rz-pdf-invoice
+Plugin URI:http://code.google.com/p/rz-pdf-invoice/
+Description: wordpress plugin for generate pdf invoice.
+Version:  0.1
+Author: Rezaur Chowdhury
+Author URI: http://www.rezaur.com
+License: MIT
+*/
+?>
